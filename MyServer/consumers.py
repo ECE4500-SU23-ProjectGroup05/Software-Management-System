b5d456adf8bb35d92d7b72c2acbd22f06df2e9bd
DavidCWQ <caowenqi4812@163.com>
2023-07-04 22:20:48 +0800

    feat (#5): add group message sending

--- MyServer/consumers.py
+++ MyServer/consumers.py
@@ -6,16 +6,24 @@ from MyServer.my_logic import compare_all, OFFICIAL_DATA, RESULT_DATA
 
 class MyConsumer(AsyncWebsocketConsumer):
 
+    client_ip = ""
+    client_mac = ""
+
     async def connect(self):
         # Perform connection setup here
         await self.accept()
+        # Add this consumer to a specific group
+        await self.channel_layer.group_add(group='clients',
+                                           channel=self.channel_name)
         await self.send(json.dumps(
             {
-                "message": "PING, a connection has established!",
+                "message": "PING, a ws connection has established!",
             },
         ))
 
     async def disconnect(self, close_code):
+        # Remove the consumer from the group
+        await self.channel_layer.group_discard('clients', self.channel_name)
         # Perform disconnection cleanup here
         print("NOTICE: A client is disconnected from the server.")
         pass
@@ -30,10 +38,16 @@ class MyConsumer(AsyncWebsocketConsumer):
             if "message" in parsed_data and parsed_data["message"] == 'PING':
                 print("Receive the greeting message from a client.")
                 await self.send('PONG and you sent: ' + text_data)
+                self.client_mac = parsed_data["mac_address"]
+                self.client_ip = parsed_data["client_ip"]
                 await self.send('DATA')
             else:
                 result = compare_all(parsed_data, OFFICIAL_DATA)
-                RESULT_DATA.append(result)
+                RESULT_DATA.append({
+                    "mac_address": self.client_mac,
+                    "client_ip": self.client_ip,
+                    "result": result,
+                })
                 print(result)
 
         except json.JSONDecodeError:
--- MyServer/my_logic.py
+++ MyServer/my_logic.py
@@ -3,6 +3,7 @@ import csv
 import json
 
 from server_side.settings import BASE_DIR
+from channels.layers import get_channel_layer
 
 # Global variable that stores the black and white list.
 OFFICIAL_DATA = []
@@ -43,3 +44,19 @@ def compare_all(client_data, official_data=None):
 
     result = client_data
     return result
+
+
+async def send_message_to_group(group_name, message='DATA'):
+    """
+    Send a message to the specific group
+    :param group_name: the group to send
+    :param message: the message to send, can be a dict
+    :return: nothing
+    """
+    channel_layer = get_channel_layer()
+    await channel_layer.group_send(
+        group_name, {
+            'type': 'web.message',
+            'message': message
+        }
+    )
