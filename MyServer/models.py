8ac7839df064cf6333bbb1d56e26a4a1c71e1153
DavidCWQ <caowenqi4812@163.com>
2023-07-08 18:53:08 +0800

    style (#10): reformat the code

--- MyServer/models.py
+++ MyServer/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 
 
@@ -25,21 +26,21 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
+
 class WhiteList(models.Model):
-    app_name = models.CharField(max_length = 256)
-    version = models.CharField(max_length = 256)
-    ip_addr = models.CharField(max_length = 256)
+    app_name = models.CharField(max_length=256)
+    version = models.CharField(max_length=256)
+    ip_addr = models.CharField(max_length=256)
 
     class Meta:
-        unique_together = ("app_name","version","ip_addr")
-    
+        unique_together = ("app_name", "version", "ip_addr")
+
 
 class UnauthorizedApp(models.Model):
-    app_name = models.CharField(max_length = 256)
-    reason = models.CharField(max_length = 256) # app reason / version reason
-    ip_addr = models.CharField(max_length = 256)
-    install_date = models.CharField(max_length = 256) #datekey format YYYYMMDD
+    app_name = models.CharField(max_length=256)
+    reason = models.CharField(max_length=256)  # app reason / version reason
+    ip_addr = models.CharField(max_length=256)
+    install_date = models.CharField(max_length=256)  # datekey format YYYYMMDD
 
     class Meta:
         unique_together = ("app_name", "ip_addr")
-    
--- MyServer/my_logic.py
+++ MyServer/my_logic.py
@@ -2,7 +2,7 @@ import os
 import csv
 import json
 
-from .models import WhiteList, UnauthorizedApp 
+from .models import WhiteList, UnauthorizedApp
 
 from server_side.settings import BASE_DIR
 from channels.layers import get_channel_layer
@@ -21,16 +21,15 @@ def read_black_white_list():
     :return: data in a dict format
     """
     file_path = os.path.join(BASE_DIR, 'MyServer', 'test.csv')
-    
 
     # Data format of the given OFFICIAL_DATA.
     data = {
-               "version": set(["1.0.1","1.0.2"]), # 'any' means all versions
-               "IP_addr": set(["195.0.0.1","195.0.0.2"]),# 0.0.0.0 means all IP address
+               "version": {"1.0.1", "1.0.2"},  # 'any' means all versions
+               "IP_addr": {"195.0.0.1", "195.0.0.2"},  # 0.0.0.0 means all IP address
            },
-    
+
     # Replace app_name with real application's name (e.g. Macfee)
-    official_data_format = { "app_name1": data,"app_name2": data}
+    official_data_format = {"app_name1": data, "app_name2": data}
 
     white_list = WhiteList.obejcts.raw("select * from MyServer_whitelist")
     for row in white_list:
@@ -41,37 +40,39 @@ def read_black_white_list():
             OFFICIAL_DATA[row.app_name] = {
                 "version": set(row.version),
                 "IP_addr": set(row.ip_addr)
-                }
+            }
     return OFFICIAL_DATA
 
 
-def compare_all(client_data,client_ip,official_data=None):
+def compare_all(client_data, client_ip, official_data=None):
     """
     This method compare the client_data with the official list according
     to its ip_address
     :return: unauthorize software name and its data in a dict format
     """
-    
+
     if official_data is None:
         official_data = OFFICIAL_DATA
     # TODO: Complete the compare logic.
     data = {
-               "version": "1.0.1",
-               "Install_date": "20010101",
-           }
-    client_data_format = {"app_name1":data, "app_name2": data}
+        "version": "1.0.1",
+        "Install_date": "20010101",
+    }
+    client_data_format = {"app_name1": data, "app_name2": data}
     client_ip_format = "195.0.0.1"
     result = {}
-    
+
     # TODO: Replace the format with real data when client_data is
     #       in the correct format
     for app_name, app_data in client_data_format.items():
         if app_name in official_data:
-            if ("0.0.0.0" in official_data[app_name]['IP_addr']) or (client_ip_format in official_data[app_name]['IP_addr']):
-                if ('any' in official_data[app_name]['version']) or (app_data['version'] in official_data[app_name]['version']):
+            if ("0.0.0.0" in official_data[app_name]['IP_addr']) or (
+                    client_ip_format in official_data[app_name]['IP_addr']):
+                if ('any' in official_data[app_name]['version']) or (
+                        app_data['version'] in official_data[app_name]['version']):
                     continue
         result[app_name] = app_data
-    
+
     return result
 
 
@@ -81,8 +82,8 @@ def query_ip(input_IP):
     format: e.g., '192.0', '192.168.0.212') in the database
     :return: all unauthorized apps where ip_addr starts with the given IP
     """
-    param = [input_IP+"%%"]
-    result = UnauthorizedApp.objects.raw("select * from MyServer_unauthorizedapp where ip_addr like %s",param)
+    param = [input_IP + "%%"]
+    result = UnauthorizedApp.objects.raw("select * from MyServer_unauthorizedapp where ip_addr like %s", param)
     return result
 
 
@@ -93,8 +94,8 @@ def query_ip_with_mask(input_IP):
     :return: all unauthorized apps and info within the given IP range
     """
     pass
-  
-  
+
+
 def export_query_result():
     """
     The method reads the data of the comparison results from the database
