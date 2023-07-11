2595a70465234f47299dc02c0cc0eb32fb5fdcdb
DavidCWQ <caowenqi4812@163.com>
2023-07-11 14:20:24 +0800

    fix (#9): the bug caused by async multi-threading

--- communication.py
+++ communication.py
@@ -1,11 +1,10 @@
 import json
+import time
 import utils
-import asyncio
+import queue
 import threading
-import websockets
+import websocket
 import data_collection
-
-from asgiref.sync import async_to_sync
 
 
 class Communication:
@@ -19,45 +18,48 @@ class Communication:
         self.ws_url = websocket_url
         self.client_data = data
         self.websocket = None
+        self.exception_q = queue.Queue()
         self.lock = threading.Lock()
-        self.send = False
+        self.running = True
 
-    async def connect_server(self, actions):
+    def connect_server(self, actions):
         """
         Continuously establishing connection with the server
-        :param actions: an async function to execute
+        :param actions: a function to execute
         :return: nothing
         """
         while True:
             try:
-                async with websockets.connect('ws://127.0.0.1:8000/ws/socket-server/') as websocket:
-                    print('WebSockets connection established.')
+                ws = websocket.create_connection('ws://127.0.0.1:8000/ws/socket-server/')
+                print('WebSockets connection established.')
 
-                    self.mac_address = data_collection.get_mac_addr()
-                    self.client_ip = data_collection.get_router_ip()
-                    self.websocket = websocket
+                self.mac_address = data_collection.get_mac_addr()
+                self.client_ip = data_collection.get_router_ip()
+                self.websocket = ws
 
-                    # Perform any initial actions or send messages after the connection is established.
-                    await websocket.send(json.dumps(
-                        {
-                            "mac_address": self.mac_address,
-                            "client_ip": self.client_ip,
-                            "message": self.greeting,
-                        },
-                    ))
+                # Perform any initial actions or send messages after the connection is established.
+                self.websocket.send(json.dumps(
+                    {
+                        "mac_address": self.mac_address,
+                        "client_ip": self.client_ip,
+                        "message": self.greeting,
+                    },
+                ))
 
-                    # Perform any subsequent actions after connection.
-                    await actions()
+                # Perform any subsequent actions after connection.
+                actions()
 
-            except websockets.ConnectionClosedError:
-                print('Connection closed. Reconnecting in 5 seconds...')
-                await asyncio.sleep(5)
+            except ConnectionResetError:
+                print('Connection Reset. Reconnecting in 5 seconds...')
+                self.running = True
+                time.sleep(5)
 
             except ConnectionRefusedError:
                 print('Connection Refused. Reconnecting in 15 seconds...')
-                await asyncio.sleep(15)
+                self.running = True
+                time.sleep(15)
 
-    async def timed_communication(self, break_time=2.5e-3):
+    def timed_communication(self, break_time=2.5e-3):
         """
         Send messages to the server (connected through websocket)
         at continuous intervals (break time)
@@ -65,57 +67,93 @@ class Communication:
         :return: nothing
         """
         self._test_connection()
-        while True:
-            await self._send_software_info()
+        while self.running:
             utils.sleep_for_some_time(break_time)
+            self._send_software_info()
 
-    async def receive_and_react(self):
+    def _timed_communication_thread(self, break_time):
+        """
+        Run timed_communication() and throw the exception to the
+        class-level queue
+        :param break_time: the break time (in hours)
+        :return: nothing
+        """
+        try:
+            self.timed_communication(break_time)
+        except Exception as e:
+            self.lock.acquire()
+            self.running = False
+            self.exception_q.put([e, "timed_communication"])
+            self.lock.release()
+
+    def receive_and_react(self):
         """
         Receive, process, and respond to the messages from the server at once
         :return: nothing
         """
         self._test_connection()
-        while True:
-            message = await self.websocket.recv()
+        while self.running:
+            message = self.websocket.recv()
             print('Received message from server:', message)
 
             # Perform the client-side logic based on server events
             if message == self.trigger:
                 # Send the data back to the server
-                await self._send_software_info()
+                self._send_software_info()
 
             # Perform any other client-side logic based on server events
             pass
 
-    def _communication_timer(self, break_time):
-        utils.sleep_for_some_time(break_time)
-        self.send = True
+    def _receive_and_react_thread(self):
+        """
+        Run receive_and_react() and throw the exception to the
+        class-level queue
+        :return: nothing
+        """
+        try:
+            self.receive_and_react()
+        except Exception as e:
+            self.lock.acquire()
+            self.running = False
+            self.exception_q.put([e, "receive_and_react"])
+            self.lock.release()
 
-    async def bidirectional_communication(self):
+    def bidirectional_communication(self):
         """
         Perform the bidirectional communication logic with the server,
         including both timed_communication and receive_and_react.
         :return: nothing
         """
         thread1 = threading.Thread(
-            target=async_to_sync(self.receive_and_react),
-            args=(),
+            target=self._timed_communication_thread,
+            args=(2.5e-3,),
             daemon=True,
         )
         thread2 = threading.Thread(
-            target=async_to_sync(self.timed_communication),
+            target=self._receive_and_react_thread,
             args=(),
             daemon=True,
         )
+        thread1.start(), thread2.start()
 
-        thread1.start()
-        thread2.start()
+        while True:
+            try:
+                # Get the exception from the queue
+                exception = self.exception_q.get(block=False)
+                print(f"Exception occurred in {exception[1]} thread: {exception[0]}")
+                # Terminate the thread cleanly
+                thread1.join(), thread2.join()
+                # Raise the exception to self.connect_server method
+                raise exception[0]
+            except queue.Empty:
+                # Continue with other tasks if no exception
+                pass
 
     def _test_connection(self):
         if self.websocket is None:
             raise Exception("ERROR: You have not established a connection to the sever!")
 
-    async def _send_software_info(self):
+    def _send_software_info(self):
         """
         Send a message about all installed software to the connected server
         :return: nothing
@@ -128,7 +166,7 @@ class Communication:
 
         # If it is the first time to send the data
         if self.client_data is None:
-            await self.websocket.send(json.dumps({
+            self.websocket.send(json.dumps({
                 "status": "update",  # "status": "new"
                 "time": utils.get_current_time(),
                 "installed": data,
@@ -141,7 +179,7 @@ class Communication:
         elif self.client_data != data:
             installed = set(data.keys()) - set(self.client_data.keys())
             uninstall = set(self.client_data.keys()) - set(data.keys())
-            await self.websocket.send(json.dumps({
+            self.websocket.send(json.dumps({
                 "status": "update",
                 "time": utils.get_current_time(),
                 "installed": {key: data[key] for key in installed},
@@ -153,5 +191,5 @@ class Communication:
         # Release the lock
         self.lock.release()
 
-    async def default_action(self, **kwargs):
+    def default_action(self, **kwargs):
         pass
