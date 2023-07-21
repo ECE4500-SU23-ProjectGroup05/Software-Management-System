9c68e6d8f236430175084cd0ffb5cad5495fc99d
QiansiqiHu <66056619+QiansiqiHu@users.noreply.github.com>
2023-07-21 23:57:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,23 +70,25 @@ elif file_up.name in ['Clash for Windows.exe', 'a5672f1287903b21f8fe4bd1851487e8
     # print(t2-t1)
     # st.write(float(t2-t1))
     # st.metric("","Time Cost:   "+str(fps)+" ms")
-# else:
-#     st.write("")
-#     st.write("Just a second...")
-#     if file_up == '':
-#         feature = data_feature[0]
-#         feature = torch.from_numpy(feature.values)
-#     else:
-#         feature = data_feature[1]
-#         feature = torch.from_numpy(feature.values)
-#     labels,fps = predict(feature,option)
-
-#     # print out the top 5 prediction labels with scores
-#     st.success('successful prediction')
-#     if labels == 1:
-#         st.write("Prediction result: the software is malicious")
-#     else:
-#         st.write("Prediction result: the software is benign")
+else:
+     
+    st.write("")
+    st.write("Just a second...")
+    time.sleep(3)
+    path = 'srlink.exe'
+    fe = extract_infos(path)
+    feature = data_feature[15003,:]
+    feature = torch.from_numpy(feature).unsqueeze(dim=0)
+    # else:
+    #     feature = data_feature[1]
+    #     feature = torch.from_numpy(feature.values)
+    labels,fps = predict(feature,option)
+        # print out the top 5 prediction labels with scores
+    st.success('successful prediction')
+    if labels == 1:
+        st.metric("","Prediction result: the software is malicious")
+    else:
+        st.metric("","Prediction result: the software is benign")
     
 #     # print(t2-t1)
 #     # st.write(float(t2-t1))
